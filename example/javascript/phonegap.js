631d68edd881b51e26b1e03c34ab23a6691e239e
Michael Brooks <michael@michaelbrooks.ca>
2010-05-24 20:48:44 -0700

    Added an example widget project.

---
+++ example/javascript/phonegap.js
@@ -0,0 +1,16 @@
+PhoneGap = { };
+navigator = { };
+
+PhoneGap.Device = {
+  platform: phonegap.device.platform,
+  version:  blackberry.system.softwareVersion,
+  name:     blackberry.system.model,
+  uuid:     phonegap.device.uuid
+};
+window.device = navigator.device = PhoneGap.Device;
+
+PhoneGap.Notification = {
+  vibrate: phonegap.notification.vibrate,
+  beep:    phonegap.notification.beep
+};
+navigator.notification = PhoneGap.Notification;
