ed60b4a2e354bd1140a75bd0c8fc9ce0f5a5670c
Justin Tyberg <jtyberg@us.ibm.com>
2010-09-24 20:41:55 -0400

    Add console.log capability to phonegap.js

--- js/phonegap.js
+++ js/phonegap.js
@@ -52,19 +52,34 @@ PhoneGap.Camera = {
 };
 navigator.camera = PhoneGap.Camera;
 
-/** 
- * This is for debug purposes only.
- * TODO: console object is not supported in BB. implement our own.
+/**
+ * phonegap.Logger is a Blackberry Widget extension that will log to the 
+ * BB Event Log and System.out.  Comment this line to disable.
+ */ 
+phonegap.Logger.enable();
+
+/**
+ * If Blackberry doesn't define a console object, we create our own.
+ * console.log will use phonegap.Logger to log to BB Event Log and System.out.
+ * Optionally, use <div/> console output for in-your-face debugging :)
+ */
 if (typeof console == "undefined") {
-	var console = new Object();
-	console.log = function(text) {
-	  if (document.getElementById("console")==null) {
-	    document.getElementsByTagName("body")[0].innerHTML = "<div id='console'></div>";
-	  }
-	  document.getElementById("console").innerHTML += "<p>" + text + "</p>";
+    
+	console = new Object();
+    
+    console.log = function(msg) {
+    	phonegap.Logger.log(msg);
+    	
+        /* just don't call console.log before the page loads 
+        if (document.getElementById("consoleOutput")==null) {
+        	var consoleDiv = document.createElement('div');
+        	consoleDiv.id = 'consoleOutput';
+        	document.getElementsByTagName("body")[0].appendChild(consoleDiv);
+        }
+        document.getElementById("consoleOutput").innerHTML += "<p>" + msg + "</p>"; 
+        */
 	}
 }
-*/
 
 /* ----- phonegap.blackberry-widgets.js ------ */
 
