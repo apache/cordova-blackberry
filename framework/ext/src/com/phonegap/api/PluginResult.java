3e0c909228248d292e9bf040632635a7a3b339cd
Justin Tyberg <jtyberg@us.ibm.com>
2010-10-03 23:08:53 -0400

    Made PluginResult extensible.
    Added ILLEGAL_ARGUMENT_EXCEPTION status.
    
    Added message field.

--- framework/ext/src/com/phonegap/api/PluginResult.java
+++ framework/ext/src/com/phonegap/api/PluginResult.java
@@ -3,8 +3,14 @@ package com.phonegap.api;
 import org.json.me.JSONObject;
 
 public class PluginResult {
+	
 	private final int status;
 	private final String message;
+	
+	public PluginResult(Status status) {
+		this.status = status.ordinal();
+		this.message = "'" + status.getMessage() + "'";
+	}
 	
 	public PluginResult(Status status, String message) {
 		this.status = status.ordinal();
@@ -43,23 +49,31 @@ public class PluginResult {
 	public static class Status
 	{
 	    private int val;
-	    private Status(int val) {
+	    private String message;
+	    
+	    protected Status(int val, String message) {
 	    	this.val = val;
+	    	this.message = message;
 	    }
 	    
 	    public int ordinal() {
 	    	return this.val;
 	    }
+	    
+	    public String getMessage() {
+	    	return this.message;
+	    }
 
-	    public static final Status OK = new Status(0);
-	    public static final Status CLASSNOTFOUNDEXCEPTION = new Status(1);
-	    public static final Status ILLEGALACCESSEXCEPTION = new Status(2);
-	    public static final Status INSTANTIATIONEXCEPTION = new Status(3);
-	    public static final Status MALFORMEDURLEXCEPTION = new Status(4);
-	    public static final Status IOEXCEPTION = new Status(5);
-	    public static final Status INVALIDACTION = new Status(6);
-	    public static final Status JSONEXCEPTION = new Status(7);
-	    public static final Status ERROR = new Status(8);
-	    public static final Status INPROGRESS = new Status(9);
+	    public static final Status OK = new Status(0, "OK");
+	    public static final Status CLASSNOTFOUNDEXCEPTION = new Status(1, "Class not found");
+	    public static final Status ILLEGALACCESSEXCEPTION = new Status(2, "Illegal access");
+	    public static final Status INSTANTIATIONEXCEPTION = new Status(3, "Instantiation error");
+	    public static final Status MALFORMEDURLEXCEPTION = new Status(4, "Malformed URL");
+	    public static final Status IOEXCEPTION = new Status(5, "IO error");
+	    public static final Status INVALIDACTION = new Status(6, "Invalid action");
+	    public static final Status JSONEXCEPTION = new Status(7, "JSON error");
+	    public static final Status ERROR = new Status(8, "Error");
+	    public static final Status ILLEGAL_ARGUMENT_EXCEPTION = new Status(9, "Illegal argument");
+	    public static final Status INPROGRESS = new Status(10, "In progress");
 	}
 }
