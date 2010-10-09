905193e962bb5e2ba22f8b0163758d1304120fb6
Justin Tyberg <jtyberg@us.ibm.com>
2010-10-09 16:51:48 -0400

    Renamed CustomDialog to AlertDialog. Cleaned up code and made dialog prettier (spacing).

--- framework/ext/src/com/phonegap/notification/AlertAction.java
+++ framework/ext/src/com/phonegap/notification/AlertAction.java
@@ -21,30 +21,36 @@ public class AlertAction {
 	private static final String DEFAULT_BUTTON  = "OK";
 	
 	/**
-	 * Displays a custom dialog box.
+	 * Displays a custom alert.
 	 *
 	 * @param args JSONArray formatted as [ message, title, buttonLabel ]
 	 *             message:     the message to display in the dialog body (default: "").
 	 *             title:       the title to display at the top of the dialog (default: "Alert").
 	 *             buttonLabel: the button text (default: "OK").
-	 * @return A CommandResult object with the success or failure state for displaying the dialog box.
+	 * @return A PluginResult object with the success or failure state for displaying the dialog box.
 	 */
 	public static PluginResult execute(JSONArray args) {
 		
 		PluginResult result = null;
 		
 		try {
-			String message     = (args.length() > 0) ? ((String)args.get(0)) : DEFAULT_MESSAGE;
-			String title       = (args.length() > 1) ? ((String)args.get(1)) : DEFAULT_TITLE;
-			String buttonLabel = (args.length() > 2) ? ((String)args.get(2)) : DEFAULT_BUTTON;
+			String message = DEFAULT_MESSAGE;
+			String title = DEFAULT_TITLE;
+			String buttonLabel = DEFAULT_BUTTON;
+			if (args.length() > 0 && args.get(0) != null)
+				message = args.getString(0);
+			if (args.length() > 1 && args.get(1) != null)
+				title = args.getString(1);
+			if (args.length() > 2 && args.get(2) != null)
+				buttonLabel = args.getString(2);
 			
 			synchronized(UiApplication.getEventLock()) {
 				UiApplication ui = UiApplication.getUiApplication();
-				Screen screen = new CustomDialog(message, title, buttonLabel);
+				Screen screen = new AlertDialog(message, title, buttonLabel);
 				ui.pushScreen(screen);
 			}
 			
-			result = new PluginResult(PluginResult.Status.OK, "OK");
+			result = new PluginResult(PluginResult.Status.OK);
 		}
 		catch (JSONException e) {
 			result = new PluginResult(PluginResult.Status.JSONEXCEPTION, "JSONException: " + e.getMessage());
