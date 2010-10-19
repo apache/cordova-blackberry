7c547b81b897e1f930a8e0a79566fbf5ebd28a99
Justin Tyberg <jtyberg@us.ibm.com>
2010-10-19 11:39:22 -0400

    Change PhoneGap version to '0.9.2'.
    Add license agreement.

--- framework/ext/src/com/phonegap/device/DeviceFeature.java
+++ framework/ext/src/com/phonegap/device/DeviceFeature.java
@@ -1,8 +1,22 @@
+/*
+ * PhoneGap is available under *either* the terms of the modified BSD license *or* the
+ * MIT License (2008). See http://opensource.org/licenses/alphabetical for full text.
+ * 
+ * Copyright (c) 2005-2010, Nitobi Software Inc.
+ * Copyright (c) 2010, IBM Corporation
+ */
 package com.phonegap.device;
 
 import net.rim.device.api.script.Scriptable;
 import net.rim.device.api.system.DeviceInfo;
 
+/**
+ * PhoneGap plugin that provides device information, including:
+ * 
+ * - Device platform version (e.g. 2.13.0.95). Not to be confused with BlackBerry OS version.
+ * - Unique device identifier (UUID).
+ * - PhoneGap software version.
+ */
 public final class DeviceFeature extends Scriptable {
 	public static final String FIELD_PLATFORM = "platform";
 	public static final String FIELD_UUID     = "uuid";
@@ -17,15 +31,9 @@ public final class DeviceFeature extends Scriptable {
 			return new Integer(DeviceInfo.getDeviceId());
 		}
 		else if (name.equals(FIELD_PHONEGAP)) {
-			return "pre-0.92 EDGE";
+			return "0.9.2";
 		}
 		
 		return super.getField(name);
 	}
 }
-
-
-
-
-
-
