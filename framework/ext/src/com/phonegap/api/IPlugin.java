e5d36c677ef0108bde43ea9b8c30d656930aabe4
Justin Tyberg <jtyberg@us.ibm.com>
2010-10-19 16:31:12 -0400

    Update LICENSE file with new text.

--- framework/ext/src/com/phonegap/api/IPlugin.java
+++ framework/ext/src/com/phonegap/api/IPlugin.java
@@ -3,6 +3,7 @@
  * MIT License (2008). See http://opensource.org/licenses/alphabetical for full text.
  * 
  * Copyright (c) 2005-2010, Nitobi Software Inc.
+ * Copyright (c) 2010, IBM Corporation
  */
 package com.phonegap.api;
 
