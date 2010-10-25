cb877e7db27be40f23bafb0320733f42137f20e3
Justin Tyberg <jtyberg@us.ibm.com>
2010-10-25 14:07:39 -0400

    Add callback to notification.alert.

--- js/notification.js
+++ js/notification.js
@@ -15,14 +15,15 @@ function Notification() {
 
 /**
  * Open a native alert dialog, with a customizable title and button text.
- * @param {String} message     Message to print in the body of the alert
- * @param {String} title       Title of the alert dialog (default: 'Alert')
- * @param {String} buttonLabel Label of the close button (default: 'OK')
+ * @param {String}   message          Message to print in the body of the alert
+ * @param {Function} completeCallback The callback that is invoked when user clicks a button.
+ * @param {String}   title            Title of the alert dialog (default: 'Alert')
+ * @param {String}   buttonLabel      Label of the close button (default: 'OK')
  */
-Notification.prototype.alert = function(message, title, buttonLabel) {
+Notification.prototype.alert = function(message, completeCallback, title, buttonLabel) {
     var _title = (title || "Alert");
     var _buttonLabel = (buttonLabel || "OK");
-    PhoneGap.exec(null, null, 'Notification', 'alert', [message, _title, _buttonLabel]);
+    PhoneGap.exec(completeCallback, null, 'Notification', 'alert', [message, _title, _buttonLabel]);
 };
 
 /**
