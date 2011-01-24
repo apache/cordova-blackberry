d11ffc3d812f9c02fc2b5e2d3593319da773caa1
Justin Tyberg <jtyberg@us.ibm.com>
2011-01-24 14:23:11 -0500

    Use application GUID and application name to log to BB Event Log.

--- framework/ext/src/com/phonegap/util/Logger.java
+++ framework/ext/src/com/phonegap/util/Logger.java
@@ -12,6 +12,8 @@ import java.util.Date;
 import net.rim.device.api.i18n.SimpleDateFormat;
 import net.rim.device.api.system.EventLogger;
 
+import com.phonegap.PhoneGapExtension;
+
 /**
  * Logger provides a mechanism to log the the Blackberry Event Log.  It uses
  * the Blackberry EventLogger class.  
@@ -27,102 +29,116 @@ import net.rim.device.api.system.EventLogger;
  */
 public class Logger {
 
-	public static final String APP_NAME = "PhoneGap";
-	public static final long GUID = 0x2ea3febe5da7e84bL; // "PhoneGap" as Long
+    /**
+     * Application name
+     */
+    protected static String appName;
+    
+    /**
+     *  Application GUID
+     */
+    protected static long appID;
 
-	// Used to format dates into a standard format
-	private static final SimpleDateFormat dateFormat = 
-		new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
-	
-	public static void enableLogging() {
-		if (EventLogger.register(GUID, APP_NAME, EventLogger.VIEWER_STRING)) {
-		    log("Logger enabled.");
-		}
-		else {
-			log("EventLogger registration failed.");
-		}
-	}
+    /**
+     *  Used to format dates into a standard format
+     */
+    private static final SimpleDateFormat dateFormat = 
+        new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
 
-	/**
-	 * Sets the minimum logging level.
-	 */	
-	public static void setMinimumLoggingLevel(int level) {
-		EventLogger.setMinimumLevel(level);
-	}
+    /**
+     * Invoke this method to enable logging to the BlackBerry Event Log.  
+     */
+    public static void enableLogging() {
+        appID = PhoneGapExtension.getAppID();
+        appName = PhoneGapExtension.getAppName(); 
+        if (EventLogger.register(appID, appName, EventLogger.VIEWER_STRING)) {
+            log("Logger enabled: " + "GUID=" + appID + ", name=" + appName);
+        }
+        else {
+            log("EventLogger registration failed.");
+        }
+    }
 
-	/**
-	 * Logs formatted message to Event Log with ALWAYS_LOG level.
-	 */
-	public static void log(String msg) {
-		logEvent(msg, EventLogger.ALWAYS_LOG);
-	}
+    /**
+     * Sets the minimum logging level.
+     */	
+    public static void setMinimumLoggingLevel(int level) {
+        EventLogger.setMinimumLevel(level);
+    }
 
-	/**
-	 * Logs formatted message to Event Log with DEBUG_INFO level.
-	 */
-	public static void debug(String msg) {
-		logEvent(msg, EventLogger.DEBUG_INFO);
-	}
+    /**
+     * Logs formatted message to Event Log with ALWAYS_LOG level.
+     */
+    public static void log(String msg) {
+        logEvent(msg, EventLogger.ALWAYS_LOG);
+    }
 
-	/**
-	 * Logs formatted message to Event Log with INFORMATION level.
-	 */
-	public static void info(String msg) {
-		logEvent(msg, EventLogger.INFORMATION);
-	}
+    /**
+     * Logs formatted message to Event Log with DEBUG_INFO level.
+     */
+    public static void debug(String msg) {
+        logEvent(msg, EventLogger.DEBUG_INFO);
+    }
 
-	/**
-	 * Logs formatted message to Event Log with WARNING level.
-	 */
-	public static void warn(String msg) {
-		logEvent(msg, EventLogger.WARNING);
-	}
+    /**
+     * Logs formatted message to Event Log with INFORMATION level.
+     */
+    public static void info(String msg) {
+        logEvent(msg, EventLogger.INFORMATION);
+    }
 
-	/**
-	 * Logs formatted message to Event Log with ERROR level.
-	 */
-	public static void error(String msg) {
-		logEvent(msg, EventLogger.ERROR);
-	}
+    /**
+     * Logs formatted message to Event Log with WARNING level.
+     */
+    public static void warn(String msg) {
+        logEvent(msg, EventLogger.WARNING);
+    }
 
-	/**
-	 * Logs formatted message to Event Log with SEVERE_ERROR level.
-	 */
-	public static void severe(String msg) {
-		logEvent(msg, EventLogger.SEVERE_ERROR);
-	}
+    /**
+     * Logs formatted message to Event Log with ERROR level.
+     */
+    public static void error(String msg) {
+        logEvent(msg, EventLogger.ERROR);
+    }
 
-	/**
-	 * Prints unformatted message to System.out.
-	 */
-	public static void out(String msg) {
-		System.out.println(msg);
-	}
+    /**
+     * Logs formatted message to Event Log with SEVERE_ERROR level.
+     */
+    public static void severe(String msg) {
+        logEvent(msg, EventLogger.SEVERE_ERROR);
+    }
 
-	/**
-	 * Prints unformatted message to System.err.
-	 */
-	public static void err(String msg, Throwable t) {
-		System.err.println(msg);
-		t.printStackTrace();
-	}
+    /**
+     * Prints unformatted message to System.out.
+     */
+    public static void out(String msg) {
+        System.out.println(msg);
+    }
 
-	/**
-	 * Logs formatted message to Event Log (if enabled) and System.out.
-	 */
-	private static void logEvent(String msg, int level) {
-		String message = formatMessage(msg);
-		EventLogger.logEvent(GUID, message.getBytes(), level);
-		out(message);
-	}
+    /**
+     * Prints unformatted message to System.err.
+     */
+    public static void err(String msg, Throwable t) {
+        System.err.println(msg);
+        t.printStackTrace();
+    }
 
-	private static String formatMessage(String msg) {
-		StringBuffer sb = new StringBuffer();
-		sb.append(APP_NAME);
-		sb.append(" [");
-		sb.append(dateFormat.format(new Date()));
-		sb.append("]: ");
-		sb.append(msg);
-		return sb.toString();
-	}
+    /**
+     * Logs formatted message to Event Log (if enabled) and System.out.
+     */
+    private static void logEvent(String msg, int level) {
+        String message = formatMessage(msg);
+        EventLogger.logEvent(appID, message.getBytes(), level);
+        out(message);
+    }
+
+    private static String formatMessage(String msg) {
+        StringBuffer sb = new StringBuffer();
+        sb.append(appName);
+        sb.append(" [");
+        sb.append(dateFormat.format(new Date()));
+        sb.append("]: ");
+        sb.append(msg);
+        return sb.toString();
+    }
 }
