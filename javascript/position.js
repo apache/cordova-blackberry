863914e34d7a6a18f275bc5c31033b14fef8847f
Justin Tyberg <jtyberg@us.ibm.com>
2011-04-08 09:41:34 -0400

    Cleanup geolocation.js.
    Make Geolocation object available through navigator.geolocation only
    (hide constructor).
    
    Keep navigator.geolocation from being defined more than once.

--- javascript/position.js
+++ javascript/position.js
@@ -4,52 +4,30 @@
  * MIT License (2008). See http://opensource.org/licenses/alphabetical for full text.
  * 
  * Copyright (c) 2005-2010, Nitobi Software Inc.
- * Copyright (c) 2010, IBM Corporation
+ * Copyright (c) 2010-2011, IBM Corporation
  */
 
 /**
  * This class contains position information.
- * @param {Object} lat
- * @param {Object} lng
- * @param {Object} acc
- * @param {Object} alt
- * @param {Object} altacc
- * @param {Object} head
- * @param {Object} vel
- * @constructor
+ * @param {Object} lat The latitude of the position.
+ * @param {Object} lng The longitude of the position.
+ * @param {Object} alt The altitude of the position.
+ * @param {Object} acc The accuracy of the position.
+ * @param {Object} head The direction the device is moving at the position.
+ * @param {Object} vel The velocity with which the device is moving at the position.
+ * @param {Object} altacc The altitude accuracy of the position.
  */
-function Position(coords, timestamp) {
-	this.coords = coords;
-    this.timestamp = timestamp;
-}
-
 function Coordinates(lat, lng, alt, acc, head, vel, altacc) {
-	/**
-	 * The latitude of the position.
-	 */
-	this.latitude = lat;
-	/**
-	 * The longitude of the position,
-	 */
-	this.longitude = lng;
-	/**
-	 * The accuracy of the position.
-	 */
-	this.accuracy = acc;
-	/**
-	 * The altitude of the position.
-	 */
-	this.altitude = alt;
-	/**
-	 * The direction the device is moving at the position.
-	 */
-	this.heading = head;
-	/**
-	 * The velocity with which the device is moving at the position.
-	 */
-	this.speed = vel;
-	/**
-	 * The altitude accuracy of the position.
-	 */
-	this.altitudeAccuracy = (altacc != 'undefined') ? altacc : null; 
-}
+    this.latitude = lat;
+    this.longitude = lng;
+    this.accuracy = acc;
+    this.altitude = alt;
+    this.heading = head;
+    this.speed = vel;
+    this.altitudeAccuracy = (altacc != 'undefined') ? altacc : null;
+};
+
+function Position(coords, timestamp) {
+    this.coords = coords;
+    this.timestamp = timestamp;
+};
