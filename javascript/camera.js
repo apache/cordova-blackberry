58f040962a5d6df371f161bc5a0fdab599ff5bc5
Justin Tyberg <jtyberg@us.ibm.com>
2011-04-08 09:26:37 -0400

    Cleanup camera.js.
    Keep Camera object from being defined more than once.
    
    Make Camera object available through navigator.camera only (hide constructor).

--- javascript/camera.js
+++ javascript/camera.js
@@ -4,91 +4,106 @@
  * MIT License (2008). See http://opensource.org/licenses/alphabetical for full text.
  * 
  * Copyright (c) 2005-2010, Nitobi Software Inc.
- * Copyright (c) 2010, IBM Corporation
+ * Copyright (c) 2010-2011, IBM Corporation
  */
 
 /**
- * This class provides access to the device camera.
- *
- * @constructor
+ * navigator.camera 
+ * 
+ * Provides access to the device camera.
  */
-Camera = function() {
-    this.successCallback = null;
-    this.errorCallback = null;
-    this.options = null;
-};
+var Camera = Camera || (function() {
+    /**
+     * Format of image that returned from getPicture.
+     *
+     * Example: navigator.camera.getPicture(success, fail,
+     *              { quality: 80,
+     *                destinationType: Camera.DestinationType.DATA_URL,
+     *                sourceType: Camera.PictureSourceType.PHOTOLIBRARY})
+     */
+    var DestinationType = {
+        DATA_URL: 0,                // Return base64 encoded string
+        FILE_URI: 1                 // Return file URI
+    };
 
-/**
- * Format of image that returned from getPicture.
- *
- * Example: navigator.camera.getPicture(success, fail,
- *              { quality: 80,
- *                destinationType: Camera.DestinationType.DATA_URL,
- *                sourceType: Camera.PictureSourceType.PHOTOLIBRARY})
- */
-Camera.DestinationType = {
-    DATA_URL: 0,                // Return base64 encoded string
-    FILE_URI: 1                 // Return file URI
-};
-Camera.prototype.DestinationType = Camera.DestinationType;
+    /**
+     * Source to getPicture from.
+     *
+     * Example: navigator.camera.getPicture(success, fail,
+     *              { quality: 80,
+     *                destinationType: Camera.DestinationType.DATA_URL,
+     *                sourceType: Camera.PictureSourceType.PHOTOLIBRARY})
+     */
+    var PictureSourceType = {       // Ignored on Blackberry
+        PHOTOLIBRARY : 0,           // Choose image from picture library 
+        CAMERA : 1,                 // Take picture from camera
+        SAVEDPHOTOALBUM : 2         // Choose image from picture library 
+    };
 
-/**
- * Source to getPicture from.
- *
- * Example: navigator.camera.getPicture(success, fail,
- *              { quality: 80,
- *                destinationType: Camera.DestinationType.DATA_URL,
- *                sourceType: Camera.PictureSourceType.PHOTOLIBRARY})
- */
-Camera.PictureSourceType = {    // Ignored on Blackberry
-    PHOTOLIBRARY : 0,           // Choose image from picture library 
-    CAMERA : 1,                 // Take picture from camera
-    SAVEDPHOTOALBUM : 2         // Choose image from picture library 
-};
-Camera.prototype.PictureSourceType = Camera.PictureSourceType;
+    /**
+     * @constructor
+     */
+    function Camera() {
+    };
 
-/**
- * Gets a picture from source defined by "options.sourceType", and returns the
- * image as defined by the "options.destinationType" option.
+    /**
+     * Attach constants to Camera.prototype (this is not really necessary, but
+     * we do it for backward compatibility).
+     */
+    Camera.prototype.DestinationType = DestinationType;
+    Camera.prototype.PictureSourceType = PictureSourceType;
+    
+    /**
+     * Gets a picture from source defined by "options.sourceType", and returns the
+     * image as defined by the "options.destinationType" option.
 
- * The defaults are sourceType=CAMERA and destinationType=DATA_URL.
- *
- * @param {Function} successCallback
- * @param {Function} errorCallback
- * @param {Object} options
- */
-Camera.prototype.getPicture = function(successCallback, errorCallback, options) {
+     * The defaults are sourceType=CAMERA and destinationType=DATA_URL.
+     *
+     * @param {Function} successCallback
+     * @param {Function} errorCallback
+     * @param {Object} options
+     */
+    Camera.prototype.getPicture = function(successCallback, errorCallback, options) {
 
-    // successCallback required
-    if (typeof successCallback != "function") {
-        console.log("Camera Error: successCallback is not a function");
-        return;
-    }
+        // successCallback required
+        if (typeof successCallback != "function") {
+            console.log("Camera Error: successCallback is not a function");
+            return;
+        }
 
-    // errorCallback optional
-    if (errorCallback && (typeof errorCallback != "function")) {
-        console.log("Camera Error: errorCallback is not a function");
-        return;
-    }
+        // errorCallback optional
+        if (errorCallback && (typeof errorCallback != "function")) {
+            console.log("Camera Error: errorCallback is not a function");
+            return;
+        }
 
-    this.successCallback = successCallback;
-    this.errorCallback = errorCallback;
-    this.options = options;
-    var quality = 80;
-    if (options.quality) {
-        quality = this.options.quality;
-    }
-    var destinationType = Camera.DestinationType.DATA_URL;
-    if (this.options.destinationType) {
-        destinationType = this.options.destinationType;
-    }
-    var sourceType = Camera.PictureSourceType.CAMERA;
-    if (typeof this.options.sourceType == "number") {
-        sourceType = this.options.sourceType;
-    }
-    PhoneGap.exec(successCallback, errorCallback, "Camera", "takePicture", [quality, destinationType, sourceType]);
-};
+        var quality = 80;
+        if (options.quality) {
+            quality = options.quality;
+        }
+        var destinationType = DestinationType.DATA_URL;
+        if (options.destinationType) {
+            destinationType = options.destinationType;
+        }
+        var sourceType = PictureSourceType.CAMERA;
+        if (typeof options.sourceType == "number") {
+            sourceType = options.sourceType;
+        }
+        PhoneGap.exec(successCallback, errorCallback, "Camera", "takePicture", [quality, destinationType, sourceType]);
+    };
 
-PhoneGap.addConstructor(function() {
-	if (typeof navigator.camera == "undefined") navigator.camera = new Camera();
-});
+    /**
+     * Define navigator.camera object.
+     */
+    PhoneGap.addConstructor(function() {
+        navigator.camera = new Camera();
+    });
+    
+    /**
+     * Return an object that contains the static constants.
+     */
+    return {
+        DestinationType: DestinationType,
+        PictureSourceType: PictureSourceType
+    };
+}());
