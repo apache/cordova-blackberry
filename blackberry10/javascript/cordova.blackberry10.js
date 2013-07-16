fd0ca93d1567f6c16e3630b3cd1cea239b8f1981
lorinbeer <lorin@adobe.com>
2013-07-16 13:58:11 -0700

    [CB-4216] updating js

--- blackberry10/javascript/cordova.blackberry10.js
+++ blackberry10/javascript/cordova.blackberry10.js
@@ -1,5 +1,5 @@
 // Platform: blackberry10
-// 3.0.0-dev-17-ge74c3f1
+// 3.0.0rc1-0-g1965a39
 /*
  Licensed to the Apache Software Foundation (ASF) under one
  or more contributor license agreements.  See the NOTICE file
@@ -19,7 +19,7 @@
  under the License.
 */
 ;(function() {
-var CORDOVA_JS_BUILD_LABEL = '3.0.0-dev-17-ge74c3f1';
+var CORDOVA_JS_BUILD_LABEL = '3.0.0rc1-0-g1965a39';
 // file: lib/scripts/require.js
 
 var require,
