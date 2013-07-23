8fa362ab83476fda502cd09b2ade09ec2ebe237d
Bryan Higgins <bryan@bryanhiggins.net>
2013-07-23 12:58:38 -0400

    [CB-4342] Detect USB connected device
    - Use 'os' module find IP address of USB connected device
    - Add new target to blackberry10.json as Model-PIN

--- blackberry10/bin/templates/project/cordova/lib/target-utils.js
+++ blackberry10/bin/templates/project/cordova/lib/target-utils.js
@@ -15,6 +15,7 @@
  */
 
 var _self,
+    os = require("os"),
     exec = require('child_process').exec,
     path = require('path'),
     bb10_utils = require('./utils'),
@@ -42,7 +43,9 @@ _self = {
                 if (count === Object.keys(targets).length) {
                     callback(targList);
                 }
-            };
+            },
+            t;
+
         if (targets) {
             for (t in targets) {
                 if (targets.hasOwnProperty(t) && targets[t].type === type) {
@@ -61,9 +64,76 @@ _self = {
         complete();
     },
 
+    getDeviceInfo: function(ip, password, callback) {
+        var cmd = path.join(process.env.CORDOVA_BBTOOLS, 'blackberry-deploy') + ' -listDeviceInfo ' + ip;
+        if (password) {
+            cmd += ' -password ' + password;
+        }
+        exec(cmd, function(error, stdout, stderr) {
+            var result = {},
+                name = /modelname::(.*?)\n/.exec(stdout),
+                pin = /devicepin::0x(.*?)\n/.exec(stdout);
+            if (name && name.length > 0) {
+                result.name = name[1];
+            }
+            if (pin && pin.length > 0) {
+                result.pin = pin[1];
+            }
+            callback(result);
+        });
+    },
+
+    findConnectedDevice: function(callback) {
+        var defaultIp = '169.254.0.1';
+        _self.discoverUsb(function (result) {
+           if (result) {
+                _self.checkConnection(result, 'device', function (connection) {
+                    if (connection)  {
+                        callback(result);
+                    } else {
+                        callback();
+                    }
+                });
+            } else {
+                _self.checkConnection(defaultIp, 'device', function (connection) {
+                    if (connection) {
+                        callback(defaultIp);
+                    } else {
+                        callback();
+                    }
+                });
+            }
+        });
+    },
+
+    discoverUsb: function(callback) {
+        var IPV4_TYPE = "IPv4",
+            IP_SPLIT_REGEXP = /(169\.254\.\d{1,3}\.)(\d{1,3})/,
+            networkInterfaces = os.networkInterfaces(),
+            result,
+            ni,
+            i;
+
+        for (ni in networkInterfaces) {
+            if (networkInterfaces.hasOwnProperty(ni)) {
+                for (i=0; i< networkInterfaces[ni].length; i++) {
+                    if (networkInterfaces[ni][i].family === IPV4_TYPE) {
+                        result = IP_SPLIT_REGEXP.exec(networkInterfaces[ni][i].address);
+                        if (result && result[1] && result[2]) {
+                            callback(result[1] + (result[2] -1));
+                            return;
+                        }
+                    }
+                }
+
+            }
+        }
+        //If we haven't found anything callback in defeat
+        callback();
+    },
+
     checkConnection: function(ip, type, callback) {
         var script = path.join(process.env.CORDOVA_BBTOOLS, 'blackberry-deploy');
-
         exec(script + ' -test ' + ip, function(error, stdout, stderr) {
             // error code 3 corresponds to a connected device, null corresponds to connected sim
             callback((type === 'simulator' && error === null) || (type == 'device' && error.code === 3));
@@ -72,8 +142,8 @@ _self = {
 
     listTargets : function(type, pruneDisconnected) {
         _self.getTargetList(type, pruneDisconnected, function (targets) {
-            for (t in targets) {
-                console.log(targets[t].name + ' ip: ' + targets[t].ip + (pruneDisconnected ? ' status: connected' : ''));
+            for (var t in targets) {
+                console.log(targets[t].name + ' ip: ' + targets[t].ip);
             }
         });
     }
