fe441beb4b8adfe98b81e4a481aeadac27a4bf44
Josh Soref <jsoref@blackberry.com>
2014-03-10 16:14:24 -0400

    CB-6241 Default to prompting for passwords (replace --query with --no-query)
    As a consequence, if you don't specify --no-query, and a password is needed,
    you will be prompted.
    
    If you have a build script which includes --query, the build will fail.

--- blackberry10/bin/templates/project/cordova/lib/build.js
+++ blackberry10/bin/templates/project/cordova/lib/build.js
@@ -45,10 +45,10 @@ function copyArgIfExists(arg) {
 }
 
 command
-    .usage('[--debug | --release] [--query] [-k | --keystorepass] [-b <number> | --buildId <number>] [-p <json> | --params <json>] [-l <level> | --loglevel <level>] [--web-inspector] [--no-signing]')
+    .usage('[--debug | --release] [--no-query] [-k | --keystorepass] [-b <number> | --buildId <number>] [-p <json> | --params <json>] [-l <level> | --loglevel <level>] [--web-inspector] [--no-signing]')
     .option('--debug', 'build in debug mode.')
     .option('--release', 'build in release mode. This will sign the resulting bar.')
-    .option('--query', 'query on the commandline when a password is needed')
+    .option('--no-query', 'fail if a password is needed and one isn\'t available')
     .option('-k, --keystorepass <password>', 'signing key password')
     .option('-b, --buildId <num>', 'specifies the build number for signing (typically incremented from previous signing).')
     .option('-p, --params <params JSON file>', 'specifies additional parameters to pass to downstream tools.')
@@ -95,7 +95,7 @@ try {
                                 done();
                             });
                         } else {
-                            err = "No signing password provided. Please enter a value for 'keystorepass' in " + pkgrUtils.homedir() + "/.cordova/blackberry10.json or use --keystorepass via command-line";
+                            err = "No signing password provided. You can omit --no-query, use --no-signing, use --keystorepass, or enter a value for 'keystorepass' in " + pkgrUtils.homedir() + "/.cordova/blackberry10.json";
                         }
                     }
                 }
